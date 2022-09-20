f52a6353cf4056be4a55da292abcc8eb5131197f
NathaliaPino <101751308+NathaliaPino@users.noreply.github.com>
2022-09-19 21:51:28 -0300

    -

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/hello", (req, res) => {
+  res.send("Hello Word");
+});
 
-app.listen(port, () => console.log (`Servidor rodando em: http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Servidor rodando em: http://localhost:${port}`)
+);
